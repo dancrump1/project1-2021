78c9b6f65d507d2064468fe808de9422dd084942
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2021-03-28 12:26:08 -0400

    day-4 just drop in links to other projects

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ const routes = [
   { name: "Home", location: "home" },
   { name: "redux store", location: "redux" },
   { name: "Animations", location: "animations" },
-  { name: "other stuff" },
+  { name: "other stuff", location: "otherStuff" },
 ];
 
 const renderNavButtons = (setCurrentRoute) => {
   return routes.map((route) => (
-      <button className="navButton" onClick={() => setCurrentRoute(route.location)}>
-        {route.name}
-      </button>
+    <button
+      className="navButton"
+      onClick={() => setCurrentRoute(route.location)}
+    >
+      {route.name}
+    </button>
   ));
 };
 
@@ -35,6 +38,19 @@ function App() {
         {currentRoute === "home" && <Home />}
         {currentRoute === "redux" && <ReduxDemonstration />}
         {currentRoute === "animations" && <AnimationsDemonstration />}
+        {currentRoute === "otherStuff" && (
+          <div>
+            <h1>
+              Please give these links a couple minutes to load. They may fail,
+              so just refresh. Heroku is free hosting, so you are waking up the
+              website when you first navigate to it
+            </h1>
+            <a href="https://lesitededan.herokuapp.com/">To my other website</a>
+            <a href="https://twitch-clip-discovery.herokuapp.com//">
+              Tool to search Twitch for clips
+            </a>
+          </div>
+        )}
       </section>
       <section className="footer">footer</section>
     </div>

b379cad68a89e3a0361946cbaeb2e2f8a8a949c7
Dan <danielcrump@outlook.com>
2021-05-30 17:46:31 -0400

    Improve styling of picture and add text

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,29 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import photo from './editedDecentPic.jpg'
-import './Home.css';
+import PropTypes from "prop-types";
+import React from "react";
+import { motion } from "framer-motion";
+import photo from "./editedDecentPic.jpg";
+import "./Home.css";
 
 const Home = () => {
-return (
-<div className="pageContainer">
-    <img src={photo} alt="something" className='profilePhoto' />
-</div>
-)
+  return (
+    <div className="pageContainer">
+      <div className="photoContainer">
+        <img src={photo} alt="something" className="profilePhoto" />
+      </div>
+      <main>
+        <p className="homePageText">
+          Welcome to my playground of front end coding attempts.
+        </p>
+        <p className="homePageText">
+          Here I hope to start playing with new ideas and fun coding projects,
+          but also hopefully use this as a staging area for all of my code to
+          come
+        </p>
+      </main>
+    </div>
+  );
 };
 
-Home.propTypes = {
-
-};
+Home.propTypes = {};
 
 export default Home;
